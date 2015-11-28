b6c694eb1ef446912e4611086a8df8233786da62
Nic Luciano <nic@kn0ll.io>
2015-11-27 20:10:38 -0500

    adding midi control and test instrument (does not help much, needs samples to get a good idea of if its working correctly)

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,14 @@
-import { Audiolet, AudioletGroup } from 'audiolet/core';
+import { Audiolet, AudioletGroup, AudioletParameter } from 'audiolet/core';
+import { Sine } from 'audiolet/dsp';
 import { PSeries } from 'audiolet/pattern';
+import { MidiGroup } from 'audiolet-midi';
 
 // for a formulaic explanation of polythythms,
 // see https://www.youtube.com/watch?v=NOWaD8Abq90
-class Generator extends AudioletGroup {
+class Generator extends MidiGroup {
 
   constructor(audiolet, beatsPerMeasure = 3, noteType = 4) {
-    super(audiolet, 0, 1);
+    super(audiolet, 0, 1, null, 0);
     this.beatsPerMeasure = beatsPerMeasure;
     this.noteType = noteType;
   }
@@ -24,16 +26,14 @@ class Generator extends AudioletGroup {
     // create a bar with n beats
     let kickBar = (() => {
       let b = [];
-      for (let i = 0; i < n; i++) {
-        b.push(1);
+      for (let i = 0; i < n * x; i++) {
+        b.push(i % x? 0: 1);
       }
       return b;
     })();
 
-    scheduler.play([new PSeries(kickBar, Infinity)], 1, (on) => {
-      if (on) {
-        // send kick
-      }
+    scheduler.play([new PSeries(kickBar, Infinity)],  1 / x, (on) => {
+      this.midiOut.send(on? 144: 128, 220, 255);
     });
 
     // accent every n subdivided notes
@@ -47,9 +47,7 @@ class Generator extends AudioletGroup {
 
     // divide each beat by x
     scheduler.play([new PSeries(snareBar, Infinity)], 1 / x, (on) => {
-      if (on) {
-        // send snare
-      }
+      this.midiOut.send(on? 144: 128, 440, 255);
     });
   }
 
@@ -57,6 +55,64 @@ class Generator extends AudioletGroup {
 
 let audiolet = new Audiolet();
 let generator = new Generator(audiolet);
-
-generator.connect(audiolet.output);
 generator.play();
+
+
+////////////
+////////////
+////////////
+
+/*
+
+class TestInstrument extends MidiGroup {
+
+  constructor(audiolet) {
+    super(audiolet, 1, 1, 0, null);
+    this.voices = new AudioletParameter(this, null, {});
+  }
+
+  addVoice(freq) {
+    let audiolet = this.audiolet;
+    let voices = this.voices;
+    let voices_value = voices.getValue();
+    let output = this.outputs[0];
+
+    if (!voices_value[freq]) {
+      let sine = new Sine(audiolet, freq);
+      voices_value[freq] = sine;
+      voices.setValue(voices_value);
+      sine.connect(output);
+    }
+  }
+
+  removeVoice(freq) {
+    let voices = this.voices;
+    let voices_value = voices.getValue();
+    let voice = voices_value[freq];
+    let output = this.outputs[0];
+
+    if (voice) {
+      delete voices_value[freq];
+      voices.setValue(voices_value);
+      voice.disconnect(output);
+    }
+  }
+
+  noteOn(key, vel) {
+    console.log('noteOn', key);
+    this.addVoice(key);
+  }
+
+  noteOff(key, vel) {
+    console.log('noteOff', key);
+    this.removeVoice(key);
+  }
+
+}
+
+let testInstrument = new TestInstrument(audiolet);
+
+generator.midiOut.connect(testInstrument);
+testInstrument.connect(audiolet.output);
+
+*/
